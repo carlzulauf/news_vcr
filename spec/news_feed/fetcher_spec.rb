d55c8169b19797396397e9435988a8c871b8805f
Carl Zulauf <carl@linkleaf.com>
2016-02-11 05:10:23 -0600

    Shorten url identifier

--- spec/news_feed/fetcher_spec.rb
+++ spec/news_feed/fetcher_spec.rb
@@ -1,15 +1,15 @@
 describe NewsFeed::Fetcher do
-  let(:rubyflow_url) { "http://www.rubyflow.com/rss" }
+  let(:url) { "http://www.rubyflow.com/rss" }
   describe "#get" do
     it "success response should return true to success?" do
       VCR.use_cassette("rubyflow") do
-        expect(subject.get(rubyflow_url)).to be_success
+        expect(subject.get(url)).to be_success
       end
     end
 
     it "failure response should return false to success?" do
       VCR.use_cassette("rubyflow_bad") do
-        expect(subject.get(rubyflow_url)).not_to be_success
+        expect(subject.get(url)).not_to be_success
       end
     end
   end
