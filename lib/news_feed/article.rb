9dfa9c84a226e386f27c2a9b87ab3636e565cce6
Carl Zulauf <carl@linkleaf.com>
2016-02-11 06:15:43 -0600

    Get article details: title, url, description

--- lib/news_feed/article.rb
+++ lib/news_feed/article.rb
@@ -3,5 +3,13 @@ module NewsFeed
     def title
       node.css("title").text
     end
+
+    def url
+      node.css("link").text
+    end
+
+    def description
+      node.css("description").text
+    end
   end
 end
--- spec/news_feed/article_spec.rb
+++ spec/news_feed/article_spec.rb
@@ -6,17 +6,32 @@ describe NewsFeed::Article do
       VCR.use_cassette("rubyflow_articles") { NewsFeed.fetch(url) }
     end
 
-    context "first" do
-      subject { doc.articles.first }
+    let(:articles) { doc.articles }
 
-      describe "title" do
-        it "should equal first title taken manually from feed" do
-          expect(subject.title).to eq(
-            "Common problems (and solutions) when starting Docker with Rails"
-          )
-        end
+    context "first" do
+      subject { articles.first }
+
+      it "should have the correct title" do
+        expect(subject.title).to eq(
+          "Common problems (and solutions) when starting Docker with Rails"
+        )
       end
 
+      it "should have the correct url" do
+        expect(subject.url).to eq(
+          "http://www.rubyflow.com/p/2s38qm-common-problems-and-solutions-when-starting-docker-with-rails"
+        )
+      end
+
+      it "should have the correct description" do
+        expect(subject.description).to eq(
+          " There are a lot of blog posts out there explaining how to get started with Docker. But in our recent experiments we’ve run into a couple of issues, so we’ve compiled a list of our encountered problems and their solutions on <a href=\"http://cookieshq.co.uk/posts/common-problems-when-starting-with-docker-and-rails/\">our latest post</a>. I hope it’s useful. "
+        )
+      end
+    end
+
+    it "should give you a console with articles if you uncomment line below" do
+      # binding.pry
     end
 
   end
