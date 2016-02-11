35a1e37b77a9cf1d48fcaed607cf6d104aeaaa99
Carl Zulauf <carl@linkleaf.com>
2016-02-11 05:02:40 -0600

    Use VCR to record success response and fake failure response

--- spec/news_feed/fetcher_spec.rb
+++ spec/news_feed/fetcher_spec.rb
@@ -1,7 +1,16 @@
 describe NewsFeed::Fetcher do
+  let(:rubyflow_url) { "http://www.rubyflow.com/rss" }
   describe "#get" do
-    it "should return a success response" do
-      expect(subject.get("http://www.rubyflow.com/rss")).to be_success
+    it "success response should return true to success?" do
+      VCR.use_cassette("rubyflow") do
+        expect(subject.get(rubyflow_url)).to be_success
+      end
+    end
+
+    it "failure response should return false to success?" do
+      VCR.use_cassette("rubyflow_bad") do
+        expect(subject.get(rubyflow_url)).not_to be_success
+      end
     end
   end
 end
