b451fd119e29a0af5584afbe57e3e2f92ddfe0ca
Carl Zulauf <carl@linkleaf.com>
2016-02-11 05:31:59 -0600

    Add document spec using nytimes cassette

--- spec/news_feed/document_spec.rb
+++ spec/news_feed/document_spec.rb
@@ -10,4 +10,16 @@ describe NewsFeed::Document do
       expect(subject.articles.count).to be > 5
     end
   end
+
+  context "with nytimes document" do
+    let(:url) { "http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml" }
+
+    subject do
+      VCR.use_cassette("nytimes") { NewsFeed.fetch(url) }
+    end
+
+    it "should have many articles" do
+      expect(subject.articles.count).to be > 5
+    end
+  end
 end
